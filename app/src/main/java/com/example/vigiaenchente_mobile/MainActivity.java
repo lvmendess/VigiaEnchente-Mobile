14494937122fb860c08cc8ba38aba03b3ed52c37
pmmc026 <paulomouracostamacedo@gmail.com>
2025-08-20 22:55:54 -0300

    botão de interação funcional

--- app/src/main/java/com/example/vigiaenchente_mobile/MainActivity.java
+++ app/src/main/java/com/example/vigiaenchente_mobile/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.vigiaenchente_mobile;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -15,10 +20,18 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String IPINFO_TOKEN = "";
+    /*private static final String IPINFO_TOKEN = "";
     private static final String OPENWHEATHER_KEY = "";
     private static final String CIDADE_DEFAULT = "Sabará";
-    private TextView clima;
+    private TextView clima;*/
+    private ImageView escudo;
+    private ImageButton button;
+    private LinearLayout statusCard;
+    private TextView statusText;
+    private int escudos[] = {R.drawable.shield_green, R.drawable.shield_yellow, R.drawable.shield_red};
+    private int statusCards[] = {R.drawable.green_background, R.drawable.yellow_background, R.drawable.red_background};
+    private String statusTexts[] = {"Não há risco de enchente", "Risco médio de enchente", "Risco alto de enchente"};
+    private int escudoAtual = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +43,23 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        escudo = findViewById(R.id.shieldIcon);
+        button = findViewById(R.id.button4);
+        statusCard = findViewById(R.id.statusCard);
+        statusText = findViewById(R.id.statusText);
         //clima = findViewById(R.id.main);
         //buscarCidadePorIp();
+    }
+
+    public void mudarEscudo(View v) {
+        if (escudoAtual >= escudos.length - 1) {
+            escudoAtual = 0;
+        } else {
+            escudoAtual++;
+        }
+        escudo.setImageResource(escudos[escudoAtual]);
+        statusCard.setBackgroundResource(statusCards[escudoAtual]);
+        statusText.setText(statusTexts[escudoAtual]);
     }
     /*
     private void buscarCidadePorIp(){
